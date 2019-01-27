700d9b9d33d355af3346522eff4060dacd34c9d1
Sophie Fischer <2315300f@student.gla.ac.uk>
2019-01-27 20:28:08 +0000

    end of chapter 8

--- rango/views.py
+++ rango/views.py
@@ -21,6 +21,10 @@ def index(request):
 
 
 def about(request):
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
     context_dict = {'boldmessage': "Rango says here is the about page"}
     return render(request, 'rango/about.html', context = context_dict)
 
